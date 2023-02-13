84df461c77b1908fc70bea3f0c663849c052d982
nglcode <nglcode@mailbox.org>
2023-02-13 12:35:33 +0100

    data type conversion

---
+++ src/com/nglcode/classandobjects/DataTypeConversion.groovy
@@ -0,0 +1,26 @@
+package com.nglcode.classandobjects
+
+class DataTypeConversion {
+
+    static main(args) {
+
+        def charValueDef = 65
+        println(charValueDef)
+
+        char charValueChar = 65
+        println(charValueChar)
+
+        def charValueConversion = 65 as char
+        println(charValueConversion)
+
+        def strNumber = "100"
+        println(strNumber.getClass().toString())
+        println(strNumber)
+
+        def strNumberConversion = "100" as int
+        println(strNumberConversion.getClass().toString())
+        println(strNumberConversion)
+    }
+
+
+}
